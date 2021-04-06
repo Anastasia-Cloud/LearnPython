2d06272e7e7907da1d5fc1d8a6f9727bf7b45c8c
AnastasiaCloud <>
2021-04-06 19:18:43 +0000

    Декартово дерево: тестирование добавленных ф-й

--- main.py
+++ main.py
@@ -2,8 +2,7 @@
 from random import random
 class DTree:
 
-  def __init__(self,key,prior):
-    #self.root=None
+  def __init__(self,key,prior=random()):
     self.key=key
     self.prior=prior
     self.left=None
@@ -25,10 +24,12 @@ class DTree:
   def Split(self,x):
     if self.key>x:
       if self.left is None:
+        print('self.left is None')
         rTree=DTree(self.key,self.prior)
         rTree.right=self.right
         rTree.left=None
         lTree=None
+        rTree.show()
         return (lTree,rTree)
       else:
         rTree=DTree(self.key,self.prior)
@@ -36,6 +37,7 @@ class DTree:
         lTree,rTree.left=self.Split(x)
     else:
       if self.right is None:
+        print('self.right is None')
         lTree=DTree(self.key,self.prior)
         lTree.left=self.left
         lTree.right=None
@@ -50,6 +52,27 @@ class DTree:
     lTree,rTree=self.Split(key)
     self=DTree.Merge(DTree.Merge(lTree,DTree(key,random())),rTree)
     print('item added!')
+
+  def show(self):
+    if self.left is not None:
+      print('left child of :',self.key,self.prior)
+      self.left.show()
+    print(self.key,self.prior)
+    if self.right is not None:
+      print('right child of :',self.key,self.prior)
+      self.right.show()
+    pass
+
+tree=DTree(15)
+tree.Add(3)
+tree.Add(33)
+tree.Add(1)
+tree.Add(45)
+tree.Add(67)
+tree.Add(111)
+tree.Add(0)
+tree.show()
+print(tree)
 
 
 
