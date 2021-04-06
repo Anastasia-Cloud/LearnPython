fe3120745a0b8c0f0f5758d2891df0c3d40cd546
AnastasiaCloud <>
2021-04-06 09:28:37 +0000

    Реализация двоичной кучи: добавление элемента

--- main.py
+++ main.py
@@ -5,7 +5,14 @@ class Heap:
     self.heapList=[]
 
   def add(self,data):
-    pass
+    self.heapList.append(data)
+    index=len(self.heapList)-1
+    parent=(index-1)//2
+    while index>0 and data>self.heapList[parent]:
+      self.heapList[index],self.heapList[parent]=self.heapList[parent],self.heapList[index]
+      index=parent
+      parent=(index-1)//2
+    print('item added!')
   
   def heapify(self,index):
     pass
@@ -18,3 +25,10 @@ class Heap:
 
   def heapSort(self,arr):
     pass
+
+myHeap=Heap()
+myHeap.add(99)
+myHeap.add(77)
+myHeap.add(6)
+myHeap.add(34)
+myHeap.add(1)
