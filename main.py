cf1280be1d782733e9e9630ed1c585fc9006f446
AnastasiaCloud <>
2021-04-06 11:17:19 +0000

    Реализация двоичной кучи: извлечение максимального элемента

--- main.py
+++ main.py
@@ -12,39 +12,67 @@ class Heap:
       self.heapList[index],self.heapList[parent]=self.heapList[parent],self.heapList[index]
       index=parent
       parent=(index-1)//2
-    print('item added!')
+    print('item added: ',self.heapList)
   
   def findMax(self,i1,i2):
+    if i2>=len(self.heapList):
+      return i1
+    if i1>=len(self.heapList):
+      return i2
     if self.heapList[i1]>self.heapList[i2]:
       return i1
     else:
       return i2
 
   def heapify(self,index):
+    if index>((len(self.heapList)-2)//2):
+      print('result: ',self.heapList)
+      return
     left=2*index+1
     right=2*index+2
     maxCh=self.findMax(left,right)
     while self.heapList[maxCh]>self.heapList[index]:
       self.heapList[index],self.heapList[maxCh]=self.heapList[maxCh],self.heapList[index]
       index=maxCh
+      if index>((len(self.heapList)-2)//2):
+        print('result: ',self.heapList)
+        return
       left=2*index+1
       right=2*index+2
       maxCh=self.findMax(left,right)
-    print('heap is ordered!')
+    print('result: ',self.heapList)
+    
 
   def buildHeap(self,arr):
-    pass
+    self.heapList=arr[:]
+    for i in range((len(self.heapList)-2)//2,-1,-1):
+      self.heapify(i)
+    print('build result: ',self.heapList)
   
   def getMax(self):
-    pass
+    if len(self.heapList)<=0:
+      return None
+    result=self.heapList[0]
+    self.heapList[0]=self.heapList[-1]
+    self.heapList.pop()
+    self.heapify(0)
+    return result
 
   def heapSort(self,arr):
     pass
 
 myHeap=Heap()
-myHeap.add(99)
-myHeap.add(77)
-myHeap.add(6)
-myHeap.add(34)
 myHeap.add(1)
-myHeap.heapify(0)
+myHeap.add(2)
+myHeap.add(3)
+myHeap.add(4)
+myHeap.add(5)
+myHeap.add(6)
+myHeap.buildHeap([1,2,3,4,0,6])
+print(myHeap.getMax())
+print(myHeap.getMax())
+print(myHeap.getMax())
+print(myHeap.getMax())
+print(myHeap.getMax())
+print(myHeap.getMax())
+print(myHeap.getMax())
