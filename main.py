52d7006adb7e500908bc34bfd8b360e3a7402f47
AnastasiaCloud <>
2021-04-07 09:20:01 +0000

    поиск ошибок

--- main.py
+++ main.py
@@ -2,11 +2,11 @@
 from random import random
 class DTree:
 
-  def __init__(self,key,prior=random()):
+  def __init__(self,key,prior=random(),l=None,r=None):
     self.key=key
     self.prior=prior
-    self.left=None
-    self.right=None
+    self.left=l
+    self.right=r
 
   @staticmethod
   def Merge(lTree,rTree):
@@ -15,41 +15,33 @@ class DTree:
     if rTree is None:
       return lTree
     if lTree.prior>rTree.prior:
-      lTree.right=DTree.Merge(lTree.right,rTree)
-      return lTree
+      newTree=DTree.Merge(lTree.right,lTree)
+      return DTree(lTree.key,lTree.prior,lTree.left,newTree)
     else:
-      rTree.left=DTree.Merge(rTree.left,lTree)
-      return rTree
+      newTree=DTree.Merge(lTree,rTree.left)
+      return DTree(rTree.key,rTree.prior,newTree,rTree.right)
+
+  
 
   def Split(self,x):
     if self.key>x:
       if self.left is None:
-        #print('self.left is None')
-        rTree=DTree(self.key,self.prior)
-        rTree.right=self.right
-        rTree.left=None
-        lTree=None
-        #rTree.show()
-        return (lTree,rTree)
+        return (None,self)
       else:
-        rTree=DTree(self.key,self.prior)
-        rTree.right=self.right
-        lTree,rTree.left=self.Split(x)
+        rTree=DTree(self.key,self.prior,None,self.right)
+        lTree,rTree.left=self.left.Split(x)
+        return (lTree,rTree)
     else:
       if self.right is None:
-        #print('self.right is None')
-        lTree=DTree(self.key,self.prior)
-        lTree.left=self.left
-        lTree.right=None
-        rTree=None
-        return (lTree,rTree)
+        return (self,None)
       else:
-        lTree=DTree(self.key,self.prior)
-        rTree.right=self.right
-        lTree.right,rTree=self.Split(x)
+        lTree=DTree(self.key,self.prior,self.left,None)
+        lTree.right,rTree=self.right.Split(x)
+        return (lTree,rTree)
 
   def Add(self,key,p=random()):
     lTree,rTree=self.Split(key)
+    '''
     print('l,r: ')
     if lTree is None:
       print('None',end=' ')
@@ -59,36 +51,40 @@ class DTree:
       print('None',end=' ')
     else:
       rTree.show()
+    '''
     res=DTree.Merge(lTree,DTree(key,p))
-    if res is None:
-      print('None',end=' ')
-    else:
-      res.show()
+
     res=DTree.Merge(res,rTree)
-    if res is None:
-      print('None',end=' ')
+    '''if res is None:
+      print('None')
     else:
-      res.show()
-    self=res
+      res.show()'''
     print('item added!')
+    return res
 
   def show(self):
     if self.left is not None:
-      print('left child of :',self.key,self.prior)
+      print('left child of ',self.key,self.prior,':',end=' ')
+      print(self.left.key,self.left.prior)
       self.left.show()
-    print(self.key,self.prior)
+    else:
+      print('left child of ',self.key,self.prior,': None')
+    #print(self.key,self.prior)
     if self.right is not None:
-      print('right child of :',self.key,self.prior)
+      print('right child of ',self.key,self.prior,':',end=' ')
+      print(self.right.key,self.right.prior)
       self.right.show()
-    pass
+    else:
+      print('right child of ',self.key,self.prior,': None')
+    
 
 tree=DTree(7,7)
-tree.Add(1,1)
-'''tree.Add(4,5)
-tree.Add(5,1)
-tree.Add(8,3)
-tree.Add(9,5)
-tree.Add(10,3)'''
+tree=tree.Add(1,1)
+tree=tree.Add(4,5)
+tree=tree.Add(5,1)
+'''tree=tree.Add(8,3)
+tree=tree.Add(9,5)
+tree=tree.Add(10,3)'''
 tree.show()
 
 
