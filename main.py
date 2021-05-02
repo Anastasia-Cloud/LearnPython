3e1a6b3df9e6b4cc8b71d6c66c3f29dfe70b0763
AnastasiaCloud <>
2021-05-02 11:09:05 +0000

    *

--- main.py
+++ main.py
@@ -15,10 +15,18 @@ print(f(6))
 '''
 
 
-
+'''
 def f(x,y=[]):
   y.append(x)
   return y
 
 print(f(5))
 print(f(6))
+'''
+
+def print_args(*args):
+  print(args)
+
+a=(1,2,3)
+print_args(a)
+print_args(*a)
